ab79c19a21b5ed5512f6760daba7381ad47b313b
Ramadan54 <ramadan.asani@edu.tbz.ch>
2025-06-27 10:51:57 +0200

    Add all classes

--- Java/2025/Sokoban/src/ch/noseryoung/Game.java
+++ Java/2025/Sokoban/src/ch/noseryoung/Game.java
@@ -1,7 +1,6 @@
 package ch.noseryoung;
 
 public class Game {
-    // Spielfeld: 0 = Boden, 1 = Wand, 2 = Box, 3 = Box auf Ziel, 4 = Ziel
     private final int[][] level = {
             {1, 1, 1, 1, 1, 1, 1},
             {1, 0, 4, 2, 0, 0, 1},
@@ -10,7 +9,6 @@ public class Game {
             {1, 1, 0, 0, 3, 3, 1},
             {1, 1, 1, 1, 1, 1, 1}
     };
-
     private int x = 2, y = 1;
 
     public int[][] getLevel() {
@@ -33,8 +31,8 @@ public class Game {
             x += dx;
             y += dy;
         } else if ((next == 2 || next == 5) && (next2 == 0 || next2 == 4)) {
-            level[y + dy][x + dx] = next - 2;        // Box-Feld wird leer
-            level[y + 2 * dy][x + 2 * dx] = next2 + 2; // neue Position bekommt Box
+            level[y + dy][x + dx] = next - 2;
+            level[y + 2 * dy][x + 2 * dx] = next2 + 2;
             x += dx;
             y += dy;
         }
